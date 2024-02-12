4eaeacb334e1ac10690814890b4fd6ae80969924
Lukasz <lukasz.wilk231@gmail.com>
2024-02-12 14:27:36 +0100

    working logging and logout with change of state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
-function App(props) {
+import {useAuthState} from "react-firebase-hooks/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJRpilIgvYbqX5RzsJ6JgYk3Oy-Mz7nCY",
+  authDomain: "chater-56a0a.firebaseapp.com",
+  projectId: "chater-56a0a",
+  storageBucket: "chater-56a0a.appspot.com",
+  messagingSenderId: "683581810189",
+  appId: "1:683581810189:web:7685ea2afd95644fb55744",
+  measurementId: "G-TZ7E8Z2B8K"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+const provider = new GoogleAuthProvider();
+
+  const logIn = () => {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    });
+  }
+
+  const logOut = () => {
+    signOut(auth).then(() => {
+      // Sign-out successful.
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+
+function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div>
-      <h1>Hello, World!</h1>
-    <button onClick={props.logIn}>Login</button>
+      <h1>Sign in</h1>
+    <button onClick={logIn}>Login</button>
+    <button onClick={logOut}>LogOut</button>
+    {user ? "elo" : "nie elo"}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,42 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-
-const provider = new GoogleAuthProvider();
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBJRpilIgvYbqX5RzsJ6JgYk3Oy-Mz7nCY",
-  authDomain: "chater-56a0a.firebaseapp.com",
-  projectId: "chater-56a0a",
-  storageBucket: "chater-56a0a.appspot.com",
-  messagingSenderId: "683581810189",
-  appId: "1:683581810189:web:7685ea2afd95644fb55744",
-  measurementId: "G-TZ7E8Z2B8K"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth();
-
-  const logIn = () => {
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user;
-    }).catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.customData.email;
-      const credential = GoogleAuthProvider.credentialFromError(error);
-    });
-  }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App logIn={logIn}/>
-  </React.StrictMode>
+    <App/>
 );
