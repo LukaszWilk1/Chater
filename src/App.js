1905ffd7d1439dd4d445527db68753112341a579
Lukasz <lukasz.wilk231@gmail.com>
2024-02-13 14:11:32 +0100

    added components and loading screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useAuthState} from "react-firebase-hooks/auth";
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider, signOut } from "firebase/auth";
 import { initializeApp } from 'firebase/app';
+import { useEffect, useState } from "react";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBJRpilIgvYbqX5RzsJ6JgYk3Oy-Mz7nCY",
@@ -16,21 +17,10 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth();
 
-const provider = new GoogleAuthProvider();
+const windowWidth = window.innerWidth;
+console.log(windowWidth);
 
-  const logIn = () => {
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user;
-    }).catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.customData.email;
-      const credential = GoogleAuthProvider.credentialFromError(error);
-    });
-  }
+const provider = new GoogleAuthProvider();
 
   const logOut = () => {
     signOut(auth).then(() => {
@@ -40,17 +30,45 @@ const provider = new GoogleAuthProvider();
     });
   }
 
-function App() {
-
-  const [user] = useAuthState(auth);
-
+function Signin(props) {
   return (
     <div className="w-100 d-flex flex-column justify-content-center p-4">
     <p className="h1 mt-4 mb-4 text-primary text-center">CHATER</p>
-    <button type="button" className="btn btn-danger w-80 mt-4 mb-4" onClick={logIn}>LOGIN WITH GOOGLE</button>
-    {user ? console.log("elo") : console.log("nie elo")}
+    <button type="button" className="btn btn-danger w-80 mt-4 mb-4" onClick={props.logIn} name="loginButton">LOGIN WITH GOOGLE</button>
     </div>
   );
+}
+
+function Chatroom() {
+  return(
+    <button onClick={logOut}>Logout</button>
+  );
+}
+
+function App() {
+
+  const logIn = () => {
+    setLoading(true);
+    signInWithPopup(auth, provider);
+    /*if(windowWidth > 768){
+      signInWithPopup(auth, provider);
+    } else if (windowWidth <= 768) {
+      signInWithRedirect(auth, provider);
+    }*/
+  }
+
+  const [user] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(false);
+  }, [user]);
+
+  return (
+    <div  className="w-100 d-flex flex-column justify-content-center p-4">
+      {loading ? <p>Loading...</p> : (user ? <Chatroom/> : <Signin logIn={logIn}/>)}
+    </div>
+  )
 }
 
 export default App;
