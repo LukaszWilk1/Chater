d86083bd80b66e7758e4a24ee6e6346d3787bb3c
Lukasz <lukasz.wilk231@gmail.com>
2024-02-16 18:34:40 +0100

    finished loading screen for chat panel component

--- src/App.js
+++ src/App.js
@@ -99,10 +99,24 @@ const ChatPanelLoading = () => {
   underRoot.classList.add("justify-content-left");
 
   return(
-    <div className="w-100 d-flex flex-column justify-content-center p-2 placeholder-glow" aria-hidden="true">
-      <p className="h1 mt-2 mb-2 text-primary text-center placeholder-glow"><span class="placeholder">WELCOME IN THE CHAT ROOM</span></p>
-      <button className="btn btn-primary w-80 mt-2 disabled placeholder w-90" aria-disabled="true"></button>
-      <button className="btn btn-primary w-80 mt-2 disabled placeholder w-90" aria-disabled="true"></button>
+    <div className="w-100 h-100 d-flex p-0" aria-hidden="true">
+      <div id="ChatPanel" className="h-100 w-20 p-2 d-flex flex-column col-3 border-end border-primary">
+        <div className="h-50 d-flex flex-column justify-content-tart placeholder-glow" aria-hidden="true">
+         <p className="h1 text-center text-primary placeholder-glow"><span className="placeholder">roomname</span></p>
+        </div>
+        <div className="h-50 d-flex flex-column justify-content-end">
+          <i className="bi bi-arrow-return-left btn btn-primary mb-1 disabled placeholder" aria-disabled="true"></i>
+          <i class="bi bi-box-arrow-left btn btn-primary disabled placeholder" aria-disabled="true"></i>
+        </div>
+      </div>
+      <div id="ChatPanel" className="h-100 w-20 p-2 d-flex flex-column col-9">
+        <div className="h-100 d-flex flex-column justify-content-end">
+        <div class="input-group">
+        <span class="placeholder col-12 bg-light m-0"></span>
+          <button class="btn btn-outline-primary disabled placeholder" type="button" id="button-addon2">Send</button>
+      </div>
+        </div>
+      </div>
     </div>
   )
 }
