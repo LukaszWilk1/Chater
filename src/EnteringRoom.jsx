540a0b45e61fa24babd5765be8bd32f1d6cc5338
Lukasz <lukasz.wilk231@gmail.com>
2024-02-18 17:26:05 +0100

    character limit for entering room input

--- src/EnteringRoom.jsx
+++ src/EnteringRoom.jsx
@@ -4,7 +4,7 @@ const EnteringRoom = prop => {
         <div className="w-100 d-flex flex-column justify-content-center p-2">
           <p className="h1 mb-5 text-primary text-center">ENTER ROOM</p>
           <div className="input-group input-group-sm mb-3">
-           <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" name="chatRoomName" onChange={prop.handleChange} value={prop.chatRoomName}></input>
+           <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" name="chatRoomName" onChange={prop.handleChange} value={prop.chatRoomName} maxLength={10}></input>
         </div>
         {prop.isEmpty ? <p className="text-danger text-center">You must name the room!</p> : <></>}
         <button className="btn btn-primary w-80 mt-2" onClick={prop.getChattRoomName}>Enter</button>
